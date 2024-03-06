4221b296c82cb87f03258905998c59557ad7d86a
Apprenant65 <maevalandrec@gmail.com>
2024-03-06 14:37:47 +0100

    first commit

---
+++ ExerciceGit/ExerciceGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciceGitApp.swift
+//  ExerciceGit
+//
+//  Created by Apprenant65 on 06/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExerciceGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
